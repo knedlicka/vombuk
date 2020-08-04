dfb8f4e5efdb482c8bc419a891b5d6e7967cc4e9
Nadka <balanchuknadiy4@gmail.com>
2020-08-04 20:00:18 +0200

    pridavanie index view in progress

--- music/views.py
+++ music/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse
+from .models import Album
 
 
 def index(request):
+    all_albums = Album.objects.all()
+    html = ''
+    for album in all_albums:
+        url = f'/music/{album.id}/'
+        html += '<a href="#">title</a><br>'
     return HttpResponse("<h1>This is the music app homepage</h1>")
+
 
 def detail(request, album_id):
     return HttpResponse("<h2>Details for Album id:" + str(album_id) + "</h2>")
