6ebe70fbc60634323e00d91ae8914769113ef409
Nadka <balanchuknadiy4@gmail.com>
2020-08-20 09:10:29 +0200

    nefunkcne favorite

--- music/views.py
+++ music/views.py
@@ -1,15 +1,12 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from .models import Album
 
 
 def index(request):
     all_albums = Album.objects.all()
-    html = ''
-    for album in all_albums:
-        url = f'/music/{album.id}/'
-        html += '<a href="#">title</a><br>'
-    return HttpResponse("<h1>This is the music app homepage</h1>")
+    return render(request, 'music/index.html', {'all_albums': all_albums})
 
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for Album id:" + str(album_id) + "</h2>")
+   album = get_object_or_404(Album, pk=album_id)
+   return render(request, 'music/detail.html', {'album': album})
